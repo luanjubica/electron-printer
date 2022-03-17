ff04b30a5e7de7af0c9e43ff48c5170faaa4823b
Luan <luan.jubica@helius-software.com>
2022-03-17 09:29:59 +0100

    feat: sample from image

--- renderer.js
+++ renderer.js
@@ -1,10 +1,13 @@
 let { remote } = require("electron");
 // console.log(process.versions.electron);
+var qrcode = require("qrcode")
 
 const { PosPrinter } = remote.require("electron-pos-printer");
 // const {PosPrinter} = require("electron-pos-printer"); //dont work in production (??)
 
 const path = require("path");
+
+
 
 let webContents = remote.getCurrentWebContents();
 let printers = webContents.getPrinters(); //list the printers
@@ -24,6 +27,17 @@ printers.map((item, index) => {
     "</label><br>";
 });
 
+qrcode.toFile('assets/img_test_qr.png', 'https://efiskalizimi-app.tatime.gov.al/invoice-check/#/verify?iic=A791ED2E856BACA3F64753BA49FAD754&tin=L91520027A&crtd=2021-10-25T12:14:06%2002:00&prc=540.00', {
+  color: {
+    dark: '#00F',  // Blue dots
+    light: '#0000' // Transparent background
+  }
+
+}, function (err) {
+  if (err) throw err
+  console.log('done')
+})
+
 
 const data = [
   {
@@ -40,7 +54,7 @@ const data = [
   },
   {
     type: "image",
-    path: path.join(__dirname, "assets/img_test.png"), // file path
+    path: path.join(__dirname, "assets/img_test_qr.png"), // file path
     position: "center", // position of image: 'left' | 'center' | 'right'
     width: "auto", // width of image in px; default: auto
     height: "60px", // width of image in px; default: 50 or '50px'
@@ -58,18 +72,18 @@ const data = [
   },
   {
     type: "barCode", // Do you think the result is ugly? Me too. Try use an image instead...
-    value: "HB4587896",
+    value: "wwww",
     height: 12,
     width: 1,
-    displayValue: true, // Display value below barcode
+    displayValue: false, // Display value below barcode
     fontsize: 8,
   },
   {
     type: "qrCode",
-    value: "https://github.com/fssonca",
-    height: 80,
-    width: 80,
-    style: "margin-left:50px",
+    value: "https://efiskalizimi-app.tatime.gov.al/invoice-check/#/verify?iic=8869F9FA4A5B435DC56AE349975DC42D&tin=K12201002T&crtd=2021-10-28T11:29:45%2B02:00&ord=306958&bu=dw963fb346&cr=jw267jw560&sw=pa302kj223&prc=34242.78",
+    height: 120,
+    width: 120,
+    style: "margin-left:10px",
   },
   {
     type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
